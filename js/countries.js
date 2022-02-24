dbf8b863f84f759383413d6508de11d606e6d52e
NishatFariza <nishatfarzia78@gmail.com>
2022-02-24 19:00:07 +0600

    rest api country name and capital

---
+++ js/countries.js
@@ -0,0 +1,29 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v2/all')
+    .then(res =>res.json())
+    .then(data =>  displayCountries(data))
+}
+loadCountries();
+
+const displayCountries = countries =>{
+    console.log(countries)
+//     for(const country of countries){
+//         console.log(country)
+//     }
+    const countriesDiv =document.getElementById('countries')
+       countries.forEach(country =>{
+               console.log(country)
+            const div =document.createElement('div')
+            div.classList.add('country')
+            const h3 =document.createElement('h3')
+            h3.innerText =country.name;
+            div.appendChild(h3);
+            const p =document.createElement('P');
+            p.innerText =country.capital;
+            div.appendChild(p);
+            countriesDiv.appendChild(div)
+
+
+       })
+
+}
